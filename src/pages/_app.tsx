cb791bbb7dbf65c6f35d737e122eb96bf460d13b
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-03-26 11:41:45 +0630

    Add: NavBar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import '@/styles/globals.css'
+import type { Session } from "next-auth"
 import { SessionProvider } from "next-auth/react"
 import type { AppProps } from 'next/app'
-import type { Session } from "next-auth"
+import NavBar from '../../components/NavBar'
 
 export default function App({
   Component,
@@ -9,7 +10,12 @@ export default function App({
 }: AppProps<{ session: Session }>) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+      <div className="bg-neutral-900 min-h-screen">
+        <NavBar />
+        <div className="md:px-10 lg:px-14 max-w-screen-xl px-4 py-6 mx-auto">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </SessionProvider >
   )
 }
