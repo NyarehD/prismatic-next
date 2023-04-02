3530cde139433be2ae62ae7084ba33bb179ad622
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-04-02 21:42:28 +0630

    Add: category create and get

---
+++ src/pages/api/category/index.ts
@@ -0,0 +1,43 @@
+import { Prisma } from '@prisma/client';
+import Joi from 'joi';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../../../prisma/prisma';
+
+// Form Validation Schema
+const formSchema = Joi.object({
+  name: Joi.string().min(3).required(),
+})
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "GET":
+      const categories = prisma.category.findMany();
+      res.status(200).json(categories);
+      break;
+
+    case "POST":
+      const { error, value } = formSchema.validate(req.body)
+      if (!error) {
+        try {
+          const newCategory = await prisma.category.create({
+            data: {
+              name: value.name
+            },
+          })
+          return res.status(200).json(newCategory)
+        } catch (e) {
+          if (e instanceof Prisma.PrismaClientKnownRequestError) {
+            if (e.code === "P2002") {
+              return res.status(400).json({ message: "User name is already exist" })
+            }
+          }
+        }
+      }
+      break;
+    default:
+      return res.status(405).json({ message: "Method is not allowed" });
+  }
+}
